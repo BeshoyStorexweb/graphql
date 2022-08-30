3f29f54f9159e54257d596dd8e969f12f3bf8993
BeshoyStorexweb <Beshoy.f@storexweb.com>
2022-08-30 17:40:57 +0200

    complete graphql setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyPraser = require("body-parser");
 const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 app.use(bodyPraser.json());
@@ -8,8 +9,31 @@ app.use(bodyPraser.json());
 app.use(
   "/graphql",
   graphqlHttp({
-    schema: null,
-    rootValue: {} // this object have all resolvers functions
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: (args) => {
+        return ["Romantic Cooking", "Sealing"];
+      },
+      createEvent: (args) => {
+        console.log(args);
+        const eventName = args.name;
+        return eventName;
+      },
+    }, // this object have all resolvers functions (logic of the schema)
+    graphiql: true,
   })
 );
 
