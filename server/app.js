a2d480d998c927f6c1a0917ed170f195036a4cc0
BeshoyStorexweb <Beshoy.f@storexweb.com>
2022-08-30 17:20:16 +0200

    setup graphql

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const bodyPraser = require("body-parser");
+const graphqlHttp = require("express-graphql");
 
 const app = express();
+app.use(bodyPraser.json());
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: null,
+    rootValue: {} // this object have all resolvers functions
+  })
+);
 
 app.listen(3000, () => {
-    console.log('Hello')
+  console.log("Hello");
 });
-app.use(bodyPraser);
