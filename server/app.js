d4ff3f6c70922f97f2e64d9e63e95215b8925751
beshoyfayez63 <beshoyfayez751@yahoo.com>
2022-08-30 22:57:57 +0200

    add models

--- server/app.js
+++ server/app.js
@@ -1,21 +1,38 @@
-const express = require("express");
-const bodyPraser = require("body-parser");
-const graphqlHttp = require("express-graphql");
-const { buildSchema } = require("graphql");
+const express = require('express');
+const bodyPraser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
 const app = express();
+const events = [];
 app.use(bodyPraser.json());
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHttp.graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        
         type RootQuery {
-            events: [String!]
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -25,16 +42,30 @@ app.use(
     `),
     rootValue: {
       events: (args) => {
-        return ["Romantic Cooking", "Sealing"];
+        return events;
       },
       createEvent: (args) => {
-        console.log(args);
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date().toISOString(),
+        };
+        events.push(event);
+        return event;
       },
     }, // this object have all resolvers functions (logic of the schema)
     graphiql: true,
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.irteb.mongodb.net/events?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+    console.log('conntected');
+  })
+  .catch((err) => console.log(err));
